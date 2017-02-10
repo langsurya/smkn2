f08c629a8a3d690d9aeacb7ea05a8dc725d63719
er1an9 <sun_eagle@ymail.com>
2017-02-11 01:41:48 +0700

    up

--- resources/views/siswa/home.blade.php
+++ resources/views/siswa/home.blade.php
@@ -5,9 +5,24 @@
   <head>
     @include('navbar')
 
-    {{-- @foreach ($data as $siswa) --}}
-      {{-- {{$data->nomor_ijazah}} --}}
-    {{-- @endforeach --}}
+    <div class="container-fluid">
+      <div class="row">
+
+        <div class="col-sm-12">
+          <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+
+            {{-- Wrapper for slides --}}
+            <div class="carousel-inner" role="listbox">
+              <div class="item active">
+                <
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
   </head>
 
 @endsection
